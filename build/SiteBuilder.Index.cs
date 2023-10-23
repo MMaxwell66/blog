75aa7aef3fdb65728b5b7c4b03f8131c80cb6525
jjshao <44551173+MMaxwell66@users.noreply.github.com>
2023-10-23 21:24:39 +0800

    [BUILD] Initial index page.

--- build/SiteBuilder.Index.cs
+++ build/SiteBuilder.Index.cs
@@ -1,7 +1,54 @@
 internal partial class SiteBuilder
 {
-	public Task BuildIndexPage()
+	public async Task BuildIndexPage()
 	{
-		return Task.CompletedTask;
+		using var output = new StreamWriter(Path.Join(OutputFolder, "index.html"));
+
+		await WriteHeader(output, MainTitle, IndexQuote);
+
+		await output.WriteAsync(
+$"""
+<header><h1><a href="{host}">{H1Title}</a></h1></header>
+<main>
+
+""");
+
+		// TODO: pagination
+		// ThenBy is just to make sure the order is consistence
+		foreach (var article in articles.OrderByDescending(article => article.PostTime).ThenBy(article => article.UrlPath))
+		{
+			await output.WriteAsync(
+$"""
+	<article>
+		<header>
+			<h2><a href="{article.UrlPath}" rel="bookmark">{article.Title}</a></h2>
+			<!-- TODO: post time & updated time -->
+			<!-- <div><span></span></div> -->
+		</header>
+		<div>
+
+""");
+			await output.WriteAsync(article.ReadLessText);
+			await output.WriteAsync(
+"""
+		</div>
+	</article>
+
+""");
+		}
+
+		await output.WriteAsync(
+"""
+	<nav>
+		<!-- TODO: pagination -->
+	</nav>
+</main>
+<footer>
+	<!-- TODO -->
+</footer>
+
+""");
+
+		await WriteFooter(output);
 	}
 }
--- build/SiteBuilder.cs
+++ build/SiteBuilder.cs
@@ -1,9 +1,17 @@
+using System.Collections.Concurrent;
+
 internal partial class SiteBuilder
 {
 	public const string ArticlesFolder = "articles";
 	public const string OutputFolder = "output";
 
+	public const string MainTitle = "JJ";
+	public const string IndexQuote = "Easy to Say, Hard to Do";
+	public const string H1Title = "JJ's blog";
+	public const string ArticleTitleSuffix = H1Title;
+
 	public readonly Uri? host;
+	private readonly ConcurrentBag<Article> articles = new();
 
 	public SiteBuilder(Uri? host)
 	{
@@ -15,14 +23,14 @@ internal partial class SiteBuilder
 		return BuildIndexPage();
 	}
 
-	private static Task WriteHeader(TextWriter output, ReadOnlySpan<char> title)
+	private static Task WriteHeader(TextWriter output, ReadOnlySpan<char> title, string suffix)
 	{
 		return output.WriteAsync($"""
 		<!DOCTYPE html>
 		<html lang="en">
 		<head>
 			<meta charset="utf-8">
-			<title>{title}</title>
+			<title>{title} | {suffix}</title>
 		</head>
 		<body>
 
@@ -32,5 +40,13 @@ internal partial class SiteBuilder
 	private static Task WriteFooter(TextWriter output)
 	{
 		return output.WriteAsync("</body>\n</html>");
+	}
+
+	private struct Article
+	{
+		public string UrlPath;
+		public string Title;
+		public DateTime PostTime;
+		public string ReadLessText;
 	}
 }
